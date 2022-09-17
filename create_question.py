d484cf0c564910de6736302555124f2d52fcd4ca
Benji Huser <benj.huser@gmail.com>
2022-09-17 11:48:09 +0200

    Add script to auto-generate question from issue

--- create_question.py
+++ create_question.py
@@ -1,9 +1,7 @@
-import glob
-import json
 import os
-import csv
+import sys
 
-# Script that creates a json file with all md files of a specified folder and all it's suboflders.
+# Script that creates a new markdown question file in the _questions folder.
 
 # -----------------------
 # prerequisite:
@@ -11,60 +9,35 @@ import csv
 # -----------------------
 
 # Inputs:
-# -
+# - Question Title
+# - Question 
 # Output:
-# questions.json and questions.csv will be created next to the script
+# question.md named according to the title passed
 
-inputDirectory = os.path.dirname(os.path.realpath(__file__))
+base_directory = os.path.dirname(os.path.realpath(__file__))
 
-outputFileJson = os.path.join(inputDirectory, "questions.json")
-outputFileCsv = os.path.join(inputDirectory, "questions.csv")
+if len(sys.argv) != 3:
+    print("Need exactly 2 inputs - Title and Question Body")
 
-print('Input Directory: ', inputDirectory)
-print('Output File json: ', outputFileJson)
-print('Output File csv: ', outputFileCsv)
+question_title = sys.argv[1]
+question_body_raw = sys.argv[2]
 
-# Possible Question File Types
-types = ('*.md', '*.markdown')
+question_body = ""
+is_content_line = False
 
-questions_directory = inputDirectory + '/_questions/'
+for line in question_body_raw.splitlines():
+    line = line.strip()
+    
+    if is_content_line:
+        question_body += "{0}\r".format(line)
+        
+    if line.startswith("---"):
+        is_content_line = True
+        
 
-questions = []
-csv_header = ['title', 'question']
+output_file = os.path.join(base_directory, "_questions", "{0}.md".format(question_title.lower().replace(" ", "_")))
 
-with open(outputFileCsv, 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(csv_header)
+print('Output File: ', output_file)
 
-    for quesiton_type in types:
-        for path in glob.glob(questions_directory + '**/' + quesiton_type, recursive=True):
-            file_name = os.path.basename(path)        
-            relative_path = path.replace(questions_directory, "").replace("\\", "/").split(".")[:-1][0]
-            print("Found {0} at path {1}".format(file_name, relative_path))
-            
-            question_title = ""
-            question_content = ""
-            question_file = open(path, "r")
-            
-            is_content_line = False
-            
-            for line in question_file:
-                if line.startswith("title:"):
-                    question_title = line.split(":")[1].strip().replace('"', "")
-                if is_content_line and line:
-                    question_content += line
-                if question_title and line.startswith("---"):
-                    is_content_line = True
-            
-            writer.writerow([question_title, question_content.replace("\r", " ").replace("\n", " ")])
-            
-            question = {
-                "path": relative_path
-            }
-            
-            questions.append(question)
-
-
-
-with open(outputFileJson, 'w') as f:
-    f.write(json.dumps(questions))
+with open(output_file, 'w') as f:
+    f.write(question_body)
